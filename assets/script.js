3f7b03c4e56ab1d093eb7fb97428fd668a029d9b
Aubrey McKinney <aubreyasdersmckinney@gmail.com>
2024-08-28 02:20:34 -0400

    tf

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,5 @@
 // write function to search the api for a random quote on button press (click event listener?), 
 // add clear button/add to fave button?
 // if so would need to use local storage & that's kind of a pain...
+var quoteSpace = docutment.getElementById("quoteSpace");
+var searchBtn = document.getElementById("searchBtn");
