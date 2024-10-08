ea2dbea255f05ee012208e61523f4bb0310a8231
annapeterson89@gmail.com <annapeterson89@gmail.com>
2024-10-07 22:24:10 -0400

    more restful routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import data from "./data.json";
 
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { cors } from "hono/cors";
 import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
+var movies: string[] = []
 
 app.use("*", cors());
 
@@ -12,15 +13,26 @@ for (const key in data) {
     if (data.hasOwnProperty(key)) {
         const movie = data[key as keyof typeof data]
         const quotes = movie.quotes
+        const topCast = movie.top_cast
         const getRandomQuote = (): string => {
             const index = Math.floor(Math.random() * quotes.length);
             return quotes[index];
         };
-        app.all(`/${key}/`, (c) => c.json({ quote: getRandomQuote() }));
-        app.all(`/${key}/text`, (c) => c.text(getRandomQuote()));
+
+        movies.push(`${key}`)
+        app.all(`/${key}/quotes/random`, (c) => c.json({ quote: getRandomQuote() }));
+        app.all(`/${key}/quotes/random/text`, (c) => c.text(getRandomQuote()));
         app.all(`/${key}/quotes`, (c) => c.json(quotes));
+        app.all(`/${key}/cast/top`, (c) => c.json(topCast));
     }
 }
+
+const getRandomMovie = (): keyof typeof data => {
+    const index = Math.floor(Math.random() * movies.length);
+    return movies[index] as keyof typeof data;
+};
+
+app.all(`/random`, (c) => c.json(data[getRandomMovie()]))
 
 app.all("*", (c) => c.notFound());
 app.onError((err, _) =>
